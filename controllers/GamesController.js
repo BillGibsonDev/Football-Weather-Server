261e69b05a57f2ac479e9732a46fde284efa7a85
Bill Gibson <71801512+BillGibsonDev@users.noreply.github.com>
2023-10-15 23:24:04 -0400

    remove older games

--- controllers/GamesController.js
+++ controllers/GamesController.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 import { GameModel } from '../models/Game.js';
 
-// inital call to get the game data
 export const getWeather = async (req, res) => { 
   try {
     const weather = await GameModel.find();
@@ -11,28 +10,45 @@ export const getWeather = async (req, res) => {
   }
 }
 
+const removeOlderData = (dateString) => {
+  const dateToCheck = new Date(dateString);
+  const today = new Date();
+
+  const dateToCheckDate = dateToCheck.toISOString().split('T')[0];
+  const todayDate = today.toISOString().split('T')[0];
+
+  return dateToCheckDate < todayDate;
+}
+
 export const addWeatherData = async (data, dayWeather, hourlyWeather) => { 
   try {
-    const game = await GameModel.findOne({ 'GameData.Gamekey': data.GameKey })
+    const game = await GameModel.findOne({ 'GameData.Gamekey': data.GameKey });
     if(game){
       game.GameData = data;
       game.GameDayWeather = dayWeather;
       game.HourlyWeather = hourlyWeather;
 
       await game.save();
-      console.log('Game Updated')
+      console.log('Game Updated');
     } else {
       await GameModel.create({
         GameData: data,
         GameDayWeather: dayWeather,
         HourlyWeather: hourlyWeather
       })
+      console.log('Game Created');
+    }
 
-      console.log('Game Created')
+    let games = await GameModel.find();
+    const filteredGames = games.filter(game => removeOlderData(game.GameData.Date));
+
+    for (const filteredGame of filteredGames) {
+      filteredGame.GameData = null;
+      await filteredGame.save();
     }
 
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
