afc4f20e4911c522610b1f941450477ffa2c6bba
Bill Gibson <71801512+BillGibsonDev@users.noreply.github.com>
2023-10-19 10:00:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.listen(port, host, () => {
   console.log(`Server active on ${port}`);
 }); 
 
-const databaseJob = new CronJob("/30 * * * *", () => {
+const databaseJob = new CronJob("*/30 * * * *", () => {
   handleGames();
   console.log('database job started');
 },
