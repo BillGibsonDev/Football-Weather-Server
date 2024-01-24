6c87965a0749aff4f3f64cb9515aeddaedd42f53
Bill Gibson <71801512+BillGibsonDev@users.noreply.github.com>
2024-01-23 23:26:42 -0500

    Update handleHourlyWeather.js

--- functions/handleHourlyWeather.js
+++ functions/handleHourlyWeather.js
@@ -8,7 +8,7 @@ export const handleHourlyWeather = async (forecastURL, data) => {
     const response = await axios.get(`${forecastURL}`, { headers: { 'User-Agent': generateUserAgent() }});
     const day = response.data.properties.periods.filter(weather => weather.startTime.slice(0, 10) === data.DateTime.slice(0, 10));
 
-    const gameStartTimeEDT = new Date(data.DateTime).toLocaleString('en-US', { timeZone: 'America/New_York' });
+    const gameStartTimeEDT = new Date(data.DateTime);
     gameStartTimeEDT.setMinutes(0);
     const gameStartTime = gameStartTimeEDT.toLocaleString('en-US', { timeZone: 'America/New_York' });
     
