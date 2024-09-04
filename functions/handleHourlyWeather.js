5ff4033efd22b66722a20d1d8269dd269e85a764
Bill Gibson <71801512+BillGibsonDev@users.noreply.github.com>
2024-09-04 17:13:32 -0400

    Update handleHourlyWeather.js

--- functions/handleHourlyWeather.js
+++ functions/handleHourlyWeather.js
@@ -14,19 +14,27 @@ export const handleHourlyWeather = async (forecastURL, data) => {
     const day = response.data.properties.periods;
 
     const gameStartTime = new Date(data.DateTime);
+    gameStartTime.setMinutes(0,0,0);
     const gameStartTimeUTC = gameStartTime.toISOString();
 
     const timeIndex = day.findIndex(weather => {
       const weatherStartTime = new Date(weather.startTime);
+      weatherStartTime.setMinutes(0,0,0);
       return weatherStartTime.toISOString() === gameStartTimeUTC;
     });
 
     if (timeIndex === -1) {
+      console.log(`Time index error: time index equals ${timeIndex}.`)
       return null;
     }
 
-    const gameEndTimeIndex = timeIndex + 8;
+    const gameEndTimeIndex = Math.min(timeIndex + 8, day.length);
     const hourlyWeather = day.slice(timeIndex + 4, gameEndTimeIndex);
+
+    if (hourlyWeather.length === 0) {
+      console.log('No weather data available in the expected range');
+      return null;
+    }
 
     const hourlyWeatherET = hourlyWeather.map(weather => {
       const weatherStartTimeUTC = new Date(weather.startTime);
